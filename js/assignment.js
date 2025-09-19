943130b35578701d31db832e16b37bb7fe217066
Clara Best <clara@Claras-MacBook-Air.local>
2025-09-19 10:14:36 -0600

    assignment finished

---
+++ js/assignment.js
@@ -0,0 +1,17 @@
+function alertMessage(message){
+    console.log(message);
+    alert(message);
+}
+function getWelcomeMessage(name){
+    const message = `Welcome ${name}`;
+    console.log(message);
+    return message;
+}
+function divide(a, b){
+    console.log(a/b);
+    return a/b;
+}
+function multiply(a,b){
+    console.log(a*b);
+    return a*b;
+}
